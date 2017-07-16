7bfec8434d0718051ffa990282239e040efb6f5e
Aditya Shinde <shinde.aditya386@gmail.com>
2017-07-15 20:06:48 -0400

    added script to initialise file_integ_watch db.

--- bin/install-db.py
+++ bin/install-db.py
@@ -2,9 +2,19 @@
 
 import os
 import sys
+import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 # Add watchman root directory to sys path to import src modules.
-sys.path.append("/".join(os.path.abspath(__file__).split("/")[:-2]))
+ROOT_DIR="/".join(os.path.abspath(__file__).split("/")[:-2])
+sys.path.append(ROOT_DIR)
 
-import src
+from src.common import dbhandler
 
+# Create db for file_integ_watch
+CONF=json.loads(open(ROOT_DIR+"/conf/file_integ_watch.conf").read())
+dbname=CONF["dbname"]
+dbpath=CONF["dbpath"]
+dbhandler.createdb_file_integ_check(dbpath+"/"+dbname)
